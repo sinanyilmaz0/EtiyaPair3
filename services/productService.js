e1ee4fe9fffe55c5570b9403ac10cfb41ef65fd0
sinanyilmaz0 <sinan.yilmaz197@gmail.com>
2022-11-18 17:59:33 +0300

    update

--- services/productService.js
+++ services/productService.js
@@ -25,11 +25,30 @@ export default class ProductService {
     }
 
     getById(id) {
+        for (const logger of this.loggers) {
+            logger.log("ProductService.getById method")
+        }
         let productToFind = this.productList.find(product => product.id === id);
         return productToFind ?? null;
     }
 
     delete(id) {
+        for (const logger of this.loggers) {
+            logger.log("ProductService.delete method")
+        }
         this.productList = this.productList.filter(product => product.id !== id);
+    }
+
+    update(product){
+        for (const logger of this.loggers) {
+            logger.log("ProductService.delete method")
+        }
+   
+        let productIndex = products.indexOf(this.productList.find(products => products.id === product.id))
+        this.productList[productIndex] = product
+       
+      
+        //this.delete(product.id)
+        //this.add(product)
     }
 }
